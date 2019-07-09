dde95db969bb7be62ef73d77591d19d75ade2282
Vanesa López <avlopez@ingenia.es>
2019-07-09 12:24:53 +0200

    Added nav, header, dashboard, twimp card

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tweempus-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {}
